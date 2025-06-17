b4712a42969c367a1fe2617bcf6648da8a866788
Rimon Kayastha <rimonkayastha@gmail.com>
2025-06-17 13:35:24 +0545

    First Commit

---
+++ python-practice.py
@@ -0,0 +1,74 @@
+import pprint
+import re
+# RANDLIST = ['A', 'B', 'C', 'D', 'E']
+# print(enumerate(RANDLIST))
+
+# ID = range(1, 11)
+# NAME = ['BOB', 'BAB', 'BIB', 'BUB', 'BEB']
+
+# for id, name in zip(ID, NAME):
+#   print(id, name)
+
+# NAME = ['BOB', 'BAB', 'BIB', 'BUB', 'BEB']
+
+# a, b = NAME[:2]
+# a, b = b, a
+# print(a, b)
+
+# PERSON_A = {
+#   'NAME':'BIB',
+#   'AGE':19,
+#   'HAIR-COLOR': 'BROWN'
+# }
+
+# print(PERSON_A.values())
+# print(PERSON_A['AGE'])
+# print(PERSON_A.items())
+# print(PERSON_A.get('HEIGHT', 'NO DATA'))
+# if 'HEIGHT' not in PERSON_A:
+#   PERSON_A.setdefault('HEIGHT', 180)
+# print(PERSON_A.get('HEIGHT'))
+# print(PERSON_A.pop('AGE'))
+# print(PERSON_A.popitem())
+# del PERSON_A['NAME']
+# print(PERSON_A.get('NAME'))
+# print(PERSON_A)
+# pprint.pprint(PERSON_A)
+# PERSON_A_BANK = {
+#   'ID': '1122',
+#   'CASH': 500
+# }
+# PERSON_A_WHOLE = {**PERSON_A, **PERSON_A_BANK}
+# for key, value in PERSON_A_WHOLE.items():
+#   print('KEY = {}, VALUE = {}'.format(key, value))
+
+# L = [(a, b) for a in range(1,4) for b in range(1,3)]
+# print(L)
+
+# a_string = "A\nB\nC"
+# print(a_string)
+# b_string = """A
+# B
+# C"""
+# print(b_string)
+
+# SPAM = b_string[0:3]
+# print(SPAM)
+
+# if SPAM.upper().isupper():
+#   print('YES')
+
+# print(' '.join(['HI', 'HOW', 'ARE', 'YOU?']))
+# print('>'.rjust(50, '-'))
+
+# test_string = re.compile(r'Spider(-)*man')
+# mo = test_string.search('Spiderman is right there')
+# print(f'{mo.group()}')  
+
+# test_string = re.compile(r'(Haha){2,4}?')
+# mo = test_string.findall('HahaHahaHahaHaha')
+# print(f'{mo}')  
+
+# test_string = re.compile(r'[^1-9aeiou]')
+# mo = test_string.findall('HUndred100')
+# print(f'{mo}')
